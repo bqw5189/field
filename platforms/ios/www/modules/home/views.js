eb43bf5b8f4134cdd98c5bb07e67eceacb9fab13
bqw_5189 <bqw_5189@163.com>
2015-03-25 15:52:43 +0800

    add home page

--- platforms/ios/www/modules/home/views.js
+++ platforms/ios/www/modules/home/views.js
@@ -2,7 +2,8 @@ field.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('field.home', {
     url: "/home",
-    templateUrl: "modules/home/page.html"
+    templateUrl: "modules/home/page.html",
+    controller:'HomeCtrl'
   })
 //  $urlRouterProvider.when('/subscribe','/subscribe/list');
 });
