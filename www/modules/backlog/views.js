23d6655ee972adf691bed2c6f3c7efd4dd733438
bqw_5189 <bqw_5189@163.com>
2015-03-30 10:50:18 +0800

    add backloged  and seach

--- www/modules/backlog/views.js
+++ www/modules/backlog/views.js
@@ -11,6 +11,24 @@ field.config(function($stateProvider, $urlRouterProvider) {
 	    templateUrl: "modules/backlog/lists.html",
 	    controller:'BacklogCtrl'
   })
+  .state('field.home.backlog.lists.backlog', {
+	    url: "/backlog",
+	    views: {
+	    	'backlog-view': {
+	    		templateUrl: "modules/backlog/lists-backlog.html",
+    	        controller:'BacklogCtrl'
+    	      }
+    	    }
+  })
+  .state('field.home.backlog.lists.backloged', {
+	    url: "/backloged",
+	    views: {
+	    	'backloged-view': {
+	    		templateUrl: "modules/backlog/lists-backloged.html",
+  	        controller:'BacklogCtrl'
+  	      }
+  	    }
+})
   .state('field.home.backlog.detail', {
 	    url: "/lists/:id",
 	    templateUrl: "modules/backlog/detail.html",
