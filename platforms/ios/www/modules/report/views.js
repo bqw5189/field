757463bdb2cb3bc3cfaa1a70e1fa25c41ce64096
bqw_5189 <bqw_5189@163.com>
2015-04-03 09:42:55 +0800

    build

---
+++ platforms/ios/www/modules/report/views.js
@@ -0,0 +1,21 @@
+field.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('field.home.report', {
+    url: "/report",
+    templateUrl: "modules/report/page.html",
+    controller:'ReportCtrl'
+    	
+  })
+  .state('field.home.report.lists', {
+	    url: "/lists/:pid",
+	    templateUrl: "modules/report/lists.html",
+	    controller:'ReportCtrl'
+  })
+  .state('field.home.report.detail', {
+	    url: "/lists/:pid/:id",
+	    templateUrl: "modules/report/detail.html",
+	    controller:'ReportCtrl'
+  })
+;
+  
+});
