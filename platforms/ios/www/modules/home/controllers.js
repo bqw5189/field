03ca0e06f045123b0dbfc579caaf20ec550f1213
bqw_5189 <bqw_5189@163.com>
2015-04-28 16:17:52 +0800

    modify date time

--- platforms/ios/www/modules/home/controllers.js
+++ platforms/ios/www/modules/home/controllers.js
@@ -2,6 +2,9 @@ angular.module('home.controllers', [])
 
 .controller('HomeCtrl', function($scope,BacklogDatas) {
 	$scope.topSlideIndex = 0;
+	$scope.nowDate = YYMMDD();
+	$scope.nowDateWeek = weekday();
+	$scope.nowDateLunar = solarDay();
 	
 	$scope.backlogsCount = function(){return BacklogDatas.length;}
 	
